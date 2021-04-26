9b513279aebe333d00edc865f5ed0756a9df24c9
NIASKA-Desktop <mkman751@yahoo.com>
2021-04-26 13:32:59 -0300

    added carousel

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,34 +1,14 @@
+import bulmaCarousel from '~bulma-carousel/dist/js/bulma-carousel.min.js';
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-        function plusSlides(n) {
-            showSlides(slideIndex += n);
-        }
-
-        function currentSlide(n) {
-            showSlides(slideIndex = n);
-        }
-
-        function showSlides(n) {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            var dots = document.getElementsByClassName("demo");
-            var captionText = document.getElementById("caption");
-            if (n > slides.length) {
-                    slideIndex = 1
-            }
-                if (n < 1) {
-                    slideIndex = slide.length
-                }
-                for(i = 0; i < slides.length; i++) {
-                    slides[i].style.display = "none";
-                }
-                for(i = 0; i < dots.length; i++) {
-                    dots[i].className = dots[i].className.replace(" active", "");
-                }
-                    slides[slideIndex-1].style.display = "block";
-                    dots[slideIndex-1].className += " active";
-                    captionText.innerHTML = dots[slideIndex-1].alt;
-                }
+bulmaCarousel.attach('#carousel-demo', {
+    slidesToScroll: 1,
+    slidesToShow: 4
+});
                 
+const carousels = bulmaCarousel.attach('.carousel', options);
+
+// To access to bulmaCarousel instance of an element
+const element = document.querySelector('#my-element');
+if (element && element.bulmaCarousel) {
+	// bulmaCarousel instance is available as element.bulmaCarousel
+}
